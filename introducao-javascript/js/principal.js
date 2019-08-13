808d798892a5d304b809ca869e6c7f3d65f86d20
Ramon Maciel <msp.ramon@gmail.com>
2019-08-12 21:56:39 -0300

    commit inicial. Valida campos e calcula imc com laço

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,40 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista"
+
+var pacientes = document.querySelectorAll(".paciente");
+for (var i = 0 ; i < pacientes.length ; i++){
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+
+    if (peso < 0 || peso > 1000) {
+        console.log("Peso Inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido!"
+    }
+
+    if (altura < 0 || altura > 3.00){
+        console.log("Altura Inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura Inválida!"
+    }
+
+    if (alturaEhValida && pesoEhValido){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
+
+}
+
